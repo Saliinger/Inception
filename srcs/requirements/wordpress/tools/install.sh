bf77e04955b437fab8adf17ae18bb0382b54d3e2
Saliinger <alexisnoukan@gmail.com>
2025-05-01 15:33:40 +0200

    add on installs

---
+++ srcs/requirements/wordpress/tools/install.sh
@@ -0,0 +1,8 @@
+!bin/bash
+
+cd /var/www
+sudo wget http://fr.wordpress.org/latest-fr_FR.tar.gz
+sudo tar -xzvf latest-fr_FR.tar.gz
+sudo rm latest-fr_FR.tar.gz
+
+# install of wordpress in the /var/www
