adb42e93844dc7b3a021244491cc61c315aabb54
Saliinger <alexisnoukan@gmail.com>
2026-03-19 17:35:52 +0100

    finish technical need to do trash readme

--- Makefile
+++ Makefile
@@ -9,14 +9,18 @@ down:
 re:			fclean all
 
 clean:
-			echo "" >> /etc/hostname
+			docker compose -f ./srcs/docker-compose.yml down
+			sed -i "/${USERNAME}.42.fr/d" /etc/hosts
 
 fclean:		clean
+			docker compose -f ./srcs/docker-compose.yml down --rmi all
+			docker compose -f ./srcs/docker-compose.yml down -v
+			rm -rf /home/${USERNAME}/data
 
 setup: 
 			mkdir -p /home/${USERNAME}/data/mariadb
 			mkdir -p /home/${USERNAME}/data/wordpress
-			echo "localhost ${USERNAME}.42.fr" >> /etc/hostname
+			grep -q "${USERNAME}.42.fr" /etc/hosts || echo "127.0.0.1 ${USERNAME}.42.fr" >> /etc/hosts
 
 ${NAME}:	setup
 			docker compose -f ./srcs/docker-compose.yml up --build -d
--- srcs/requirements/wordpress/script/run.sh
+++ srcs/requirements/wordpress/script/run.sh
@@ -10,9 +10,7 @@ REGULAR_USER=$(sed -n '4p'  /run/secrets/credential)
 REGULAR_EMAIL=$(sed -n '5p'  /run/secrets/credential)
 REGULAR_PASSWORD=$(sed -n '6p' /run/secrets/credential)
 
-until [ mysqladmin ping ]; do
-	sleep 1
-done
+until mysqladmin ping --host=${DB_HOST} --silent; do sleep 1; done
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O wp-cli.phar
@@ -21,8 +19,8 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	mv wp-cli.phar /usr/local/bin/wp
 
 	wp core download --path=/var/www/html --allow-root
-	wp config create --dbhost ${DB_HOST} --dbname ${MYSQL_DATABASE} --dbuser ${MYSQL_USER} --dbpass ${DB_PASSWORD} --allow-root
-	wp core install --url ${DOMAIN_NAME} --admin_user ${ADMIN_USER} --admin_email ${ADMIN_EMAIL} --admin_password ${ADMIN_PASSWORD} --skip-email --allow-root
+	wp config create --path=/var/www/html --dbhost ${DB_HOST} --dbname ${MYSQL_DATABASE} --dbuser ${MYSQL_USER} --dbpass ${DB_PASSWORD} --allow-root
+	wp core install --path=/var/www/html --title "Inception" --url ${DOMAIN_NAME} --admin_user ${ADMIN_USER} --admin_email ${ADMIN_EMAIL} --admin_password ${ADMIN_PASSWORD} --skip-email --allow-root
 	wp user create ${REGULAR_USER} ${REGULAR_EMAIL} --user_pass ${REGULAR_PASSWORD} --role=editor --allow-root
 
 fi
